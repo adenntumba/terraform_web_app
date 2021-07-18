1d27f7f5cb76781e640105dee30f75e763440be7
Adenn Tumba <adinn.tumba@hotmail.com>
2021-07-17 22:02:31 -0300

    Adiciona o App

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>Index - PHP</title>
+ </head>
+ <body style="background-color:powderblue;">
+ <?php 
+ echo "<center><h1>MENTORIA IaC</h1></center>";
+ echo "<center><h3>Aprenda de forma prática em projetos reais</h3></center>"; 
+ ?>
+ </body>
+</html>
